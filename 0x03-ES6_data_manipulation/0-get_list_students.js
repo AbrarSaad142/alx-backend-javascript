9353130763cf1c70663b5e7b9ffe6c2c95f66e56
Abrar Saad <abrarsaad3572022@gmail.com>
2024-07-30 17:12:01 +0300

    1. More mapping

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,18 @@
 export default function getListStudents() {
-  const first = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-  const second = { id: 2, firstName: 'James', location: 'Columbia' };
-  const third = { id: 3, firstName: 'Serena', location: 'San Francisco' };
+  const first = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+  const second = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+  const third = {
+    id: 3,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
   return [first, second, third];
 }
